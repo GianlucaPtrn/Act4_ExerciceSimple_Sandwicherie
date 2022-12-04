533c8a33b113a0ce165c5e14f9c46b7a1163aef1
GianlucaPtrn <200188@site.asty-moulin.be>
2022-12-04 22:31:57 +0100

    add files

---
+++ Act4_ExerciceSimple_SandwichMaker/Act4_ExerciceSimple_SandwichMaker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Act4_ExerciceSimple_SandwichMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMaker sandwich = new SandwichMaker();
+            string reload = "";
+
+            Console.WriteLine("Bienvenu dans notre concepteur de de sandwich !!");
+            Console.WriteLine();
+
+            Console.WriteLine(" Vous composer un sandwich ! ");
+            Console.WriteLine();
+
+            Console.WriteLine(SandwichMaker.SentenceSandwich());
+            Console.WriteLine();
+
+            while (reload == "oui")
+            {
+                Console.WriteLine("Voulez vous composer un sandwich ? oui: 'oui' et non: n'importe quelle autre touche !");
+                Console.WriteLine(" -> oui: 'oui' ");
+                Console.WriteLine(" -> non: appuyer sur n'importe quelle autre touche !");
+                reload = Console.ReadLine();
+
+                Console.WriteLine(SandwichMaker.SentenceSandwich());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Act4_ExerciceSimple_SandwichMaker/Act4_ExerciceSimple_SandwichMaker/SandwichMaker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Act4_ExerciceSimple_SandwichMaker
+{
+    class SandwichMaker
+    {
+		private string[] _proteine = new string[4] {"steak", "hamburger", "poison", "jambon"};
+		private string[] _condiments = new string[4] { "moyonnaise", "ketchup", "andalouse", "fromagère" };
+		private string[] _pain = new string[4] { "pain blanc", "pain gris", "pain complet", "baguette" };
+		private string[] _crudités = new string[4] { "salade", "tomate", "cornichon", "ognion" };
+
+        public string[] Proteine
+        {
+			get{ 
+				return _proteine; 
+			}
+			set{
+                _proteine = value; 
+			}
+		}
+        public string[] Condiments
+        {
+            get{
+                return _condiments;
+            }
+            set{
+                _condiments = value;
+            }
+        }
+        public string[] Pain
+        {
+            get{
+                return _pain;
+            }
+            set{
+                _pain = value;
+            }
+        }
+        public string[] Crudités
+        {
+            get
+            {
+                return _crudités;
+            }
+            set
+            {
+                _crudités = value;
+            }
+        }
+
+        public SandwichMaker(string[] proteine, string[] condiments, string[] pain, string[] crudités)
+        {
+            this._proteine = proteine;
+            this._condiments = condiments;
+            this._pain = pain;
+            this._crudités = crudités;
+        }
+
+        public string SentenceSandwich(string _proteine, string _condiments, string _pain, string _crudités)
+        {
+            for(int s = 0; s < 4; s++){
+                Random nbrAlea = new Random();
+                int chooseIngr = nbrAlea.Next(4);
+             
+                if(s == 0)
+                {
+                    _proteine = Proteine[chooseIngr];
+                }
+                else if (s == 1)
+                {
+                    _condiments = Condiments[chooseIngr];
+                }
+                else if (s == 2)
+                {
+                    _pain = Pain[chooseIngr];
+                }
+                else if (s == 3)
+                {
+                    _crudités = Crudités[chooseIngr];
+                }
+            }
+            
+            string sentence = "voici votre sandwich : " + Pain + "," + Proteine + "," + Condiments + "," + Crudités; 
+            return sentence;
+        }
+    }
+}
